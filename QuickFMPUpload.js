f74081424fe8fb1a17688794cba99dde1d4dee1a
Hub-Del-Imac Server <hub-studio-server@Hub-Studio-Servers-Mac-Studio.local>
2024-11-21 11:15:00 -0500

    Rewrote the program to work as a recursive function
    Working as a recursive function, able to identify uncreated directories in the ftp server and create them

--- QuickFMPUpload.js
+++ QuickFMPUpload.js
@@ -1,101 +1,87 @@
+//THE SACRED TEXTS: Node-FTP github https://github.com/mscdex/node-ftp
+
 const fs = require('fs');
-
-const express = require('express');
-var app = express();
-
-//Node-FTP github https://github.com/mscdex/node-ftp
-const Client = require('ftp'); 
+const path = require('path');
+const Client = require('ftp');
 const HubTracker_Config = require('./HubTracking_Config.js') //Change this to wherever the config is stored
-const fmpAuthentication = HubTracker_Config.fmpAuthentication_Config;
 const ftpConfig = HubTracker_Config.FTPConnector_Config;
 
 //----------Logic Start---------
 
 ///Volumes/Server/PODs/UploadFTP
 
-var baseDir = '/Volumes/Server/PODs/Upload FTP'
+const baseDir = '/Volumes/Server/PODs/Upload FTP'
 
-var files = CreateUploadObject(baseDir)
+const ftpClient = new Client();
+ftpClient.on('ready', function()
+{
+    uploadToFMP(baseDir, ftpClient, "PODs");
+    ftpClient.end();
+});
+ftpClient.connect(ftpConfig);
 
-if(files.length == 0)
+//-----Functions----
+
+//Recursive function, will keep self calling until no directories are found within the file tree
+function uploadToFMP(filePath, ftpClient, ftpPath)
 {
-    console.log("Nothing to upload, exiting")
-}
-else
-{
-    var ftpClient = new Client();
-    ftpClient.on('ready', function() {
-    
-        files.forEach(file =>
+    /*
+    Steps...
+    1 - Get a list of file within the path
+    2 - Loop each file & check validity
+    3 - If it's a directory - Check to see if the matching directory exists within the FTP server
+        -If not, create it and call the function for the directory found
+        -If so, call this function for the directory found
+    4 - If it's a file, upload to the ftp server
+     */
+    const files = fs.readdirSync(filePath);
+    files.forEach(file =>
+    {
+        //Checks the file extension for either .jpg or a directory
+        let fileExtension = path.extname(file);
+        if((fileExtension === '.jpg' || fileExtension === '') && file !== ".DS_Store")
         {
-            var FPTLocation = "PODs/" + file.year + "/" + file.month + "/" + file.day + "/" + file.filename
-            console.log("Uploading " + file.filename + " to " + FPTLocation)
-            ftpClient.put(file.path, FPTLocation, function(err) 
+            //Grabs a list of all the files at the current FTP path
+            //NEEDS to be queued up regardless of it's a directory or not, this way we can make sure that the directory exists when we move to upload a file
+            //This works because it makes sure that the mkdir is queued before any files found
+            ftpClient.list(ftpPath, function(err, list)
             {
                 if (err) throw err;
-                ftpClient.end();
-            });
-        })
-        console.log("Finished")
-    });
-    ftpClient.connect(ftpConfig);
-}
-
-
-//Sweeps through the toupload folder and creates a list of objects for each file to upload
-function CreateUploadObject(path)
-{
-    var filePaths = []; //yyyy/mm/dd/file.ext
-
-    var yearsFS = fs.readdirSync(path);
-    //Gets all the years in the base folder
-    yearsFS.forEach(year =>
-    {
-        yearPath = path + "/" + year
-        var yearStats = fs.statSync(yearPath)
-        if(yearStats.isDirectory() && year != "Upload FTP.app")
-        {
-            var monthFS = fs.readdirSync(yearPath)
-            monthFS.forEach(month =>
-            {
-                monthPath = yearPath + "/" + month;
-                var monthStats = fs.statSync(monthPath)
-                if(monthStats.isDirectory())
+                let fileData = fs.statSync(filePath + "/" + file);
+                //I can't figure out why FS thinks Upload FTP.app is a directory
+                if(fileData.isDirectory() && file)
                 {
-                    var dayFS = fs.readdirSync(monthPath)
-                    dayFS.forEach(day =>
+                    let found = false;
+                    //If it's a directory, check the FTP for a matching directory
+                    list.forEach(value =>
                     {
-                        dayPath = monthPath + "/" + day;
-                        var dayStats = fs.statSync(dayPath)
-                        if(dayStats.isDirectory())
+                        if(value.name === file)
                         {
-                            var podFS = fs.readdirSync(dayPath)
-                            podFS.forEach(pod =>
-                            {
-                                var podPath = dayPath + "/" + pod
-                                var podStats = fs.statSync(podPath)
-
-                                if(podStats.isFile())
-                                {
-                                    var filedata =
-                                    {
-                                        path: podPath,
-                                        year: year,
-                                        month: month,
-                                        day: day,
-                                        filename: pod
-                                    }
-
-                                    filePaths.push(filedata)
-                                }
-                            })
+                            found = true;
+                            //Repeat function for the found directory
+                            console.log(`Found folder at ${filePath + "/" + file}`);
+                            uploadToFMP(filePath + "/" + file, ftpClient, ftpPath + "/" + file);
                         }
-
                     })
+                    if(!found)
+                    {
+                        //File wasn't found, create a directory then repeat the function for the found directory
+                        let newPath =  ftpPath + "/" + file
+                        console.log(`Creating directory ${newPath}`);
+                        ftpClient.mkdir(newPath, function(err) { if (err) throw err })
+                        uploadToFMP(filePath + "/" + file, ftpClient, newPath);
+                    }
+                }
+                else
+                {
+                    //If it's not a directory, upload it to the FTP server
+                    ftpClient.put(filePath + "/" +file, ftpPath + "/" + file, function(err)
+                    {
+                        console.log(`Uploading ${file} to ${ftpPath}`);
+                        if (err) throw err;
+                    });
                 }
             })
         }
     })
-
-    return filePaths
 }
